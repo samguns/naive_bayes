f5d72e749152d7f1c9787dae8e3c786f462aa7c6
samguns <wang.vai@gmail.com>
2018-07-26 22:57:30 +0800

    Initial draft.

---
+++ classifier.h
@@ -0,0 +1,38 @@
+//
+// Created by Sam on 2018/7/26.
+//
+
+#ifndef NAIVE_BAYES_CLASSIFIER_H
+#define NAIVE_BAYES_CLASSIFIER_H
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+class GNB {
+ public:
+
+  vector<string> possible_labels = {"left","keep","right"};
+
+
+  /**
+    * Constructor
+    */
+  GNB();
+
+  /**
+   * Destructor
+   */
+  virtual ~GNB();
+
+  void train(vector<vector<double> > data, vector<string>  labels);
+
+  string predict(vector<double>);
+
+};
+
+#endif //NAIVE_BAYES_CLASSIFIER_H
